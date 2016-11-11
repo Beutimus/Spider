d9880a7535aecaad4bf5f1c8400a122b10c9bb62
Andrew Airmet <andrewa@nwri.com>
2016-11-11 15:26:42 -0700

    Initial commit for jpg comparison tests.

---
+++ src/Tests/JPEGComparison.java
@@ -0,0 +1,12 @@
+package Tests;
+
+public class JPEGComparison {
+
+	String baseFile = "file:/C:/Users/Owner/workspace/STGSpider/rawKitten.jpg";
+	String quality10FilePath = "file:/C:/Users/Owner/workspace/STGSpider/rawKitten10.jpg";
+	String quality30FilePath = "file:/C:/Users/Owner/workspace/STGSpider/rawKitten30.jpg";
+	String quality70FilePath = "file:/C:/Users/Owner/workspace/STGSpider/rawKitten70.jpg";
+	String quality100FilePath = "file:/C:/Users/Owner/workspace/STGSpider/rawKitten100.jpg";
+	
+	
+}
