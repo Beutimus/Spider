315e70378c06cccfc32b87d3ecd0bcead940dbc0
Andrew Airmet <andrewa@nwri.com>
2016-11-15 14:16:59 -0700

    Initial pass on the audio file testing.

---
+++ src/Tests/AudioFileTests.java
@@ -0,0 +1,70 @@
+package Tests;
+
+import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+
+import org.junit.Assert;
+import org.testng.annotations.Test;
+
+public class AudioFileTests {
+
+	public static String somePathName = "C:/Users/Owner/workspace/STGSpider/Recording.aiff";
+
+	@Test
+	public void audioTest()
+	{
+		int totalFramesRead = 0;
+		File fileIn = new File(somePathName);
+
+		// somePathName is a pre-existing string whose value was
+		// based on a user selection.
+		try 
+		{
+			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(fileIn);
+			int bytesPerFrame = audioInputStream.getFormat().getFrameSize();
+
+			if (bytesPerFrame == AudioSystem.NOT_SPECIFIED) {
+				// some audio formats may have unspecified frame size
+				// in that case we may read any amount of bytes
+				bytesPerFrame = 1;
+			} 
+			
+			System.out.println("Bytes per frame: " + bytesPerFrame);
+
+			// Set an arbitrary buffer size of 1024 frames.
+			int numBytes = 1024 * bytesPerFrame; 
+			byte[] audioBytes = new byte[numBytes];
+
+			try {
+				int numBytesRead = 0;
+				int numFramesRead = 0;
+				// Try to read numBytes bytes from the file.
+				while ((numBytesRead = audioInputStream.read(audioBytes)) != -1) {
+					// Calculate the number of frames actually read.
+					numFramesRead = numBytesRead / bytesPerFrame;
+					totalFramesRead += numFramesRead;
+					// Here, do something useful with the audio data that's 
+					// now in the audioBytes array...
+					System.out.println(audioBytes);
+					
+					for (byte bytes : audioBytes)
+					{
+						System.out.println(bytes);
+					}
+					
+					
+				}
+			} catch (Exception ex) { 
+				ex.printStackTrace();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			Assert.fail();
+		}
+		
+		System.out.println("Total frames read: " + totalFramesRead);
+	}
+
+}
